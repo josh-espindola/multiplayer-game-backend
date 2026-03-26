06047475edbc4c515bfbe4d15be4e1165d268ad1
Joshua Espindola <jespindolaarellano@gmail.com>
2026-03-26 19:01:02 -0300

    feat: Socket- persistencia de conexión en el servidor + middleware de autenticación

--- socket/handlers/connectionHandler.js
+++ socket/handlers/connectionHandler.js
@@ -1,7 +1,29 @@
+/* Gestión de todo lo que sucede mientras el socket esta conectado */
+
+let players = {};
+
 const connectionHandler = (io, socket)=>{
-    console.log("Socket conectado :", socket.id);
+
+    socket.on("player:join",(data)=>{
+        players[socket.id] ={
+            userId: data.userId,
+            username: data.username
+        };
+        io.emit("players:update",Object.values(players));
+        console.log("jugadores activos", players)
+    })
+
+    socket.on("players:get",()=>{
+        //Enviar lista de jugadores actuales
+        socket.emit("players:update",Object.values(players))
+    })
+
     socket.on("disconnect",()=>{
         console.log("Usuario desconectado",socket.id);
+
+        delete players[socket.id];
+        io.emit("players:update",Object.values(players));
+        console.log("jugadores activos",players)
     })
 
 }
--- socket/socket.js
+++ socket/socket.js
@@ -1,19 +1,35 @@
 import { Server } from 'socket.io';
 import { connectionHandler } from './handlers/connectionHandler.js';
 import { chatHandler } from './handlers/chatHandler.js';
+import { verifyToken } from '../utils/jwt.js';
+import AppError from '../utils/AppError.js'
 
 const initSocket = (httpServer) => {
     /* Inicialización del servidor */
     const io = new Server(httpServer, {
         cors: {
             origin: process.env.FRONTEND_URL
+        },
+    })
+    io.use((socket, next) => {
+        try {
+            const token = socket.handshake.auth.token;
+            if (!token) throw new AppError("Token required", 401);
+
+            const payload = verifyToken(token); // devuelve id, username, etc.
+            socket.user = payload; // 🔹 guardamos info del usuario en el socket
+            next();
+        } catch (err) {
+            next(err);
         }
     })
+
 
     io.on("connection", (socket) => {
         connectionHandler(io, socket);
         chatHandler(io, socket);
     })
+
 
 }
 
